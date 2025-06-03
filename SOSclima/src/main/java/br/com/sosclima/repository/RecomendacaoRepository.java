a1a85d4cbe15075b156c766c673060d3dd1d63ae
EnRico-94-git <141572064+EnRico-94-git@users.noreply.github.com>
2025-06-03 15:26:10 -0300

    Delete SOSclima directory

--- SOSclima/src/main/java/br/com/sosclima/repository/RecomendacaoRepository.java
+++
@@ -1,8 +0,0 @@
-package br.com.sosclima.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import br.com.sosclima.model.Recomendacao;
-
-public interface RecomendacaoRepository extends JpaRepository<Recomendacao, Long> {
-}
