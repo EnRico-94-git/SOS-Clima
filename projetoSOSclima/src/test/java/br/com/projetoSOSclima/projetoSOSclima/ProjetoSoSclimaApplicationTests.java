8b3bd051d1f973576b68414dbf697de4536e81ba
EnRico-94-git <141572064+EnRico-94-git@users.noreply.github.com>
2025-06-05 18:36:17 -0300

    Add files via upload

---
+++ projetoSOSclima/src/test/java/br/com/projetoSOSclima/projetoSOSclima/ProjetoSoSclimaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.projetoSOSclima.projetoSOSclima;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoSoSclimaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
