3f905a07e7ca9e34542d8f0f999a71a135da3a35
EnRico-94-git <141572064+EnRico-94-git@users.noreply.github.com>
2025-06-06 11:19:54 -0300

    Delete projeto-musica directory

--- projeto-musica/src/main/java/br/com/fiap/projeto_musica/respository/LocalRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.fiap.projeto_musica.respository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import br.com.fiap.projeto_musica.model.Local;
-
-public interface LocalRepository extends JpaRepository<Local, Long>{
-
-}
--- projeto-musica/src/main/java/br/com/fiap/projeto_musica/respository/RecomendacaoRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.fiap.projeto_musica.respository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import br.com.fiap.projeto_musica.model.Recomendacao;
-
-public interface RecomendacaoRepository extends JpaRepository<Recomendacao, Long>{
-
-}
